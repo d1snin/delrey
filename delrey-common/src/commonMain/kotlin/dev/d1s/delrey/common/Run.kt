848bc25ff9b33da184c4f3d4fd1ecdde87274a8e
d1snin <me@d1s.dev>
2023-09-28 02:34:51 +0300

    feat: add common entities

---
+++ delrey-common/src/commonMain/kotlin/dev/d1s/delrey/common/Run.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 Mikhail Titov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.d1s.delrey.common
+
+import kotlinx.serialization.Serializable
+
+public typealias RunId = String
+
+public typealias ExitCode = Int
+public typealias Output = String
+
+public interface AbstractRun
+
+@Serializable
+public data class Run(
+    val id: RunId,
+    val command: Command,
+    val status: ExitCode?,
+    val output: Output
+) : AbstractRun
+
+@Serializable
+public data class ModifiedRun(
+    val command: Command
+) : AbstractRun
+
+@Serializable
+public data class AcceptedRun(
+    val id: RunId
+) : AbstractRun
