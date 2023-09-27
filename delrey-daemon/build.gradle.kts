ad44ec288c03d56e98c1c68caa06f24568017492
d1snin <me@d1s.dev>
2023-09-28 00:24:33 +0300

    feat: init

---
+++ delrey-daemon/build.gradle.kts
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2023 Mikhail Titov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    application
+    id("com.github.ben-manes.versions")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies {
+    val logbackVersion: String by project
+    val kmLogVersion: String by project
+
+    val hopliteVersion: String by project
+
+    val koinVersion: String by project
+
+    val dispatchVersion: String by project
+
+    implementation(project(":delrey-common"))
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("org.lighthousegames:logging:$kmLogVersion")
+
+    implementation("com.sksamuel.hoplite:hoplite-core:$hopliteVersion")
+
+    implementation("io.insert-koin:koin-core:$koinVersion")
+    implementation("io.insert-koin:koin-logger-slf4j:$koinVersion")
+
+    implementation("com.rickbusarow.dispatch:dispatch-core:$dispatchVersion")
+}
+
+application {
+    mainClass.set("dev.d1s.delrey.daemon.MainKt")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.majorVersion
+    }
+}
---
+++ delrey-master/build.gradle.kts
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2023 Mikhail Titov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the; License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+    id("io.ktor.plugin")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    val exktVersion: String by project
+
+    val ktorVersion: String by project
+    val ktorWsEventsVersion: String by project
+
+    val logbackVersion: String by project
+    val kmLogVersion: String by project
+
+    val koinVersion: String by project
+
+    val dispatchVersion: String by project
+
+    implementation("dev.d1s.exkt:exkt-common:$exktVersion")
+    implementation("dev.d1s.exkt:exkt-ktor-server:$exktVersion")
+    implementation("dev.d1s.exkt:exkt-ktor-server-koin:$exktVersion")
+    implementation("dev.d1s.exkt:exkt-konform:$exktVersion")
+
+    implementation(project(":delrey-common"))
+
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jwt:$ktorVersion")
+    implementation("io.ktor:ktor-server-rate-limit:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-server-cors:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("dev.d1s.exkt:exkt-ktor-server:$exktVersion")
+    implementation("dev.d1s.ktor-ws-events:ktor-ws-events-server:$ktorWsEventsVersion")
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("org.lighthousegames:logging:$kmLogVersion")
+
+    implementation("io.insert-koin:koin-ktor:$koinVersion")
+    implementation("io.insert-koin:koin-logger-slf4j:$koinVersion")
+
+    implementation("com.rickbusarow.dispatch:dispatch-core:$dispatchVersion")
+}
+
+application {
+    mainClass.set("dev.d1s.delrey.master.MainKt")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.majorVersion
+    }
+}
+
+ktor {
+    docker {
+        localImageName.set(project.name)
+    }
+}
