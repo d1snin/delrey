6b58012d62e56c72ffbfd62da57877985c48c679
d1snin <me@d1s.dev>
2023-10-10 23:21:49 +0300

    fix: check compatibility on daemon startup

--- delrey-daemon/src/main/kotlin/dev/d1s/delrey/daemon/DelreyDaemonApplication.kt
+++ delrey-daemon/src/main/kotlin/dev/d1s/delrey/daemon/DelreyDaemonApplication.kt
@@ -1,15 +1,42 @@
 package dev.d1s.delrey.daemon
 
+import dev.d1s.delrey.client.DelreyMasterClient
+import dev.d1s.delrey.common.VERSION
 import dev.d1s.delrey.daemon.service.SessionListener
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import org.lighthousegames.logging.logging
 
 class DelreyDaemonApplication : KoinComponent {
 
     private val sessionListener by inject<SessionListener>()
 
+    private val client by inject<DelreyMasterClient>()
+
+    private val log = logging()
+
     suspend fun run() {
+        log.i {
+            "Delrey Daemon v$VERSION is starting..."
+        }
+
+        checkCompatibility()
+
         val job = sessionListener.listen()
         job.join()
+    }
+
+    private suspend fun checkCompatibility() {
+        val status = client.getStatus().getOrThrow()
+
+        val masterVersion = status.version
+
+        log.i {
+            "Delrey Master Server version: $masterVersion; State: ${status.state}"
+        }
+
+        if (VERSION != status.version) {
+            error("This Delrey Daemon binary is not compatible with Delrey Master server")
+        }
     }
 }
